234f01e97e8c701da11756063bd84f423a064cbd
Турсункулов Хабибулло Дилшодович <110659455+TursunkulovKhabib@users.noreply.github.com>
2026-06-06 18:49:46 +0300

    fix: import quiz.css in main.jsx so AI Quiz styles apply

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import ScrollToTop from "./components/ScrollToTop";
 import "./styles/global.css";
+import "./styles/quiz.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
